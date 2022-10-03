6acf8bcbf273149c0e3b37853942f7df426c8fb7
Nico Mandel <mandel.nicolas@gmail.com>
2022-10-03 09:59:44 +0200

    simplified test file

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,27 +1,31 @@
 # to test imports
 import pytest
-import torch
 import warnings
 
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
+
+def test_torch():
+    import torch
+
+def test_flash():
+    import flash 
+
+def test_cuda():
+    import torch
+    assert torch.cuda.is_available()
+
+def test_cuda_version():
+    import torch
+    cuda_v = float(torch.version.cuda)
+    assert cuda_v > 11.0
     
-    def test_flash():
-        import flash 
 
-    def test_cuda():
-        assert torch.cuda.is_available()
+def test_arch():
+    import torch
+    rtx_arch = 'sm_86'
+    archlist = torch.cuda.get_arch_list()
+    assert rtx_arch in archlist
 
-    def test_cuda_version():
-        cuda_v = float(torch.version.cuda)
-        assert cuda_v > 11.0
-        
-
-    def test_arch():
-        rtx_arch = 'sm_86'
-        archlist = torch.cuda.get_arch_list()
-        assert rtx_arch in archlist
-
-    def test_device():
-        torch.cuda.get_device_name(0)
+def test_device():
+    import torch
+    torch.cuda.get_device_name(0)
 
