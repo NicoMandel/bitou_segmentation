509371c77dc5bb8c9cbd5ea3f491c107b663f112
Nico Mandel <mandel.nicolas@gmail.com>
2022-10-14 10:38:09 +0200

    added plotting for basic prediction file

--- scripts/predict.py
+++ scripts/predict.py
@@ -1,5 +1,6 @@
 # The basic semantic segmentation as outlined in the pytorch flash documentation [here](https://lightning-flash.readthedocs.io/en/latest/reference/semantic_segmentation.html)
 
+from base64 import decode
 import torch
 import numpy as np
 
@@ -8,6 +9,7 @@ import flash
 from flash.image import SemanticSegmentation, SemanticSegmentationData
 
 from PIL import Image
+import matplotlib.pyplot as plt
 
 colour_code = np.array([(220, 220, 220), (128, 0, 0), (0, 128, 0),  # class
                         (192, 0, 0), (64, 128, 0), (192, 128, 0)])  # background
@@ -46,28 +48,48 @@ trainer = flash.Trainer(max_epochs=3, gpus=torch.cuda.device_count())
 # 4. Segment a few images!
 predict_files = [
         "data/bitou_test/DJI_20220404135614_0001.JPG",
-        # "data/bitou_test/DJI_20220404140510_0015.JPG",
-        # "data/bitou_test/DJI_20220404140802_0022.JPG"
+        "data/bitou_test/DJI_20220404140510_0015.JPG",
+        "data/bitou_test/DJI_20220404140802_0022.JPG"
     ]
 datamodule = SemanticSegmentationData.from_files(
     predict_files=predict_files,
     batch_size=1
 )
-predictions = trainer.predict(model, datamodule=datamodule, output='preds') # using 'labels' does not work - unknown why. class names?
+predictions = trainer.predict(model, datamodule=datamodule, output='preds') # using 'labels' does not work - unknown why. class names? output='preds'
 print(predictions)
-pred = predictions[0][0]
-print(pred.shape)
-label = torch.argmax(pred.squeeze(), dim=0).detach().numpy()
-# label = (pred > 0.0).float()
-print(label.shape)
-decoded = decode_colormap(label.detach().numpy(), num_classes=2)
+for i, pred in enumerate(predictions):
+    pr = pred[0]
+    print(pr.shape)
+    label = torch.argmax(pr.squeeze(), dim=0).detach().numpy()
+    l = (pr > 0.0).float()
+    lab = torch.argmax(pr, dim=0).detach().numpy()
+    print(label.shape)
+    dec_label = decode_colormap(label, num_classes=2)
+    dec_l = decode_colormap(l)
+    dec_lab = decode_colormap(lab)
 
-# 6. Show the images
-imf = predict_files[0] 
-im = Image.open(imf)
-im.show()
+    # 6. Show the images
+    imf = predict_files[i] 
+    im = Image.open(imf)
+    im_arr = np.asarray(im)
 
-# show the predictions
-im_lab = Image.fromarray(decoded)
-im_lab.show()
+    # show the predictions
+    fig, axs = plt.subplots(2,2)    
+    axs[0,0].imshow(im_arr)
+    axs[0,0].set_title("Original")
+    axs[0,0].axis('off')
+
+    axs[0,1].imshow(dec_label)
+    axs[0,1].set_title("Squeeze")
+    axs[0,1].axis('off')
+
+    # axs[1,0].imshow(dec_l)
+    # axs[1,0].set_title("larger than 0")
+    # axs[1,0].axis('off')
+
+    axs[1,1].imshow(dec_lab)
+    axs[1,1].set_title("No squeeze")
+    axs[1,1].axis('off')
+    plt.show()
+
 
