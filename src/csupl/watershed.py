98418c2683fca7602a0215eeb361e1ac8e8e3383
Nico Mandel <mandel.nicolas@gmail.com>
2022-11-17 10:29:17 +0100

    watershed class pipeline base form 1

--- src/csupl/watershed.py
+++ src/csupl/watershed.py
@@ -6,12 +6,20 @@
 
 import cv2
 import numpy as np
+from csupl.k_means import km_algo
 
 
 class Watershed:
 
     def __init__(self) -> None:
-        pass
+        # TODO: initialise the best k_means classifier here
+        # TODO afterwards - use for pre-labelling
+        # TODO afterwards - generate masks with pre-labelled data & overlay from hand-generated mask
+        fpath = "results/kmeans/classifiers/kmeans_K-3_scale-20_hsv_full.pkl"
+        self.classif = km_algo.load_classifier(fpath)
+        self.sand_label = 1
+        self.tolerance = 1.0
+
 
     def __call__(self, img : np.ndarray, markers : np.ndarray) -> np.ndarray:
         """
@@ -25,7 +33,7 @@ class Watershed:
                 * unknown should be marked with 0 
         """
         markers = self.__preprocess(img)
-        out_img = cv2.watershed(img, markers)
+        out_img = cv2.watershed(img, markers.astype(np.int32))
         out_img = self.__postprocess(out_img)
         return out_img
 
@@ -33,10 +41,17 @@ class Watershed:
         """
             Use preprocess to generate marker proposals
         """
-        pass
+        _, label = self.classif.calculate_distance(img, tol = self.tolerance)
+        return label
 
     def __postprocess(self, out_img : np.ndarray) -> np.ndarray:
         """
-            Use postprocess to return colour codes and other util
+            Use postprocess to return colour codes and other util.
+            Watershed output:
+                * 0 = unknown
+                * -1 = border
+                * rest are classes
         """
-        pass
+        kernel = np.ones((5,5), np.uint8)
+        n_img = cv2.dilate(out_img.astype(np.int32), kernel, 1)
+        return n_img
