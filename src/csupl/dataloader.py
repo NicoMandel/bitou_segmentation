d27ea51fbeeff9da4d29892f216d63bafa66c47c
Nico Mandel <mandel.nicolas@gmail.com>
2022-11-24 18:28:35 +0100

    unified dataloaders

--- src/csupl/dataloader.py
+++ src/csupl/dataloader.py
@@ -12,6 +12,7 @@ from typing import Any, Callable, Optional
 # For albumentations
 import cv2
 import numpy as np
+import os.path
 
 import pytorch_lightning as pl
 
@@ -123,3 +124,78 @@ class BitouDataModule(pl.LightningDataModule):
     # def test_dataloader(self):
         # dl = DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=self.num_workers, pin_memory=True)
         # return dl
+
+#################################
+# Secondary Datasets
+#################################
+
+# Adopted from the OG OxfordPetDataset
+class OxfordPetDataset(torch.utils.data.Dataset):
+    def __init__(self, root, mode="train", transform=None):
+
+        self.root = root
+        self.mode = mode
+        self.transform = transform
+
+        self.images_directory = os.path.join(self.root, "orig")
+        self.masks_directory = os.path.join(self.root, "mask")
+
+        perc = 0.25
+        self.filenames = self._read_split(perc)
+
+    def __len__(self):
+        return len(self.filenames)
+
+    def __getitem__(self, idx):
+
+        filename = self.filenames[idx]
+        image_path = os.path.join(self.images_directory, filename + ".JPG")
+        mask_path = os.path.join(self.masks_directory, filename + ".JPG")
+
+        image = np.array(Image.open(image_path).convert("RGB"))
+
+        # have to reduce the channel here from 3 to 1
+        trimap = np.array(Image.open(mask_path))[... , 0]
+        mask = self._preprocess_mask(trimap)
+
+        sample = dict(image=image, mask=mask, trimap=trimap)
+        if self.transform is not None:
+            sample = self.transform(**sample)
+
+        return sample
+
+    @staticmethod
+    def _preprocess_mask(mask):
+        mask = mask.astype(np.float32)
+        mask[mask == 2.0] = 0.0
+        mask[(mask == 1.0) | (mask == 3.0)] = 1.0
+        return mask
+
+    def _read_split(self, perc):
+        inv_perc = int(1. / perc)
+        imdir = Path(self.images_directory)
+        filenames = list([x.stem for x in imdir.glob("*.JPG")])
+        # filenames = [x.split(" ")[0] for x in split_data]
+        if self.mode == "train":  # % for train
+            filenames = [x for i, x in enumerate(filenames) if i % inv_perc != 0]
+        elif self.mode == "valid":  # % for validation
+            filenames = [x for i, x in enumerate(filenames) if i % inv_perc == 0]
+        return filenames
+
+# adopted from the Simple OxfordPetDataset
+class SimpleBitouPetDataset(OxfordPetDataset):
+    def __getitem__(self, *args, **kwargs):
+
+        sample = super().__getitem__(*args, **kwargs)
+
+        # resize images
+        image = np.array(Image.fromarray(sample["image"]).resize((256, 256), Image.LINEAR))
+        mask = np.array(Image.fromarray(sample["mask"]).resize((256, 256), Image.NEAREST))
+        trimap = np.array(Image.fromarray(sample["trimap"]).resize((256, 256), Image.NEAREST))
+
+        # convert to other format HWC -> CHW
+        sample["image"] = np.moveaxis(image, -1, 0)
+        sample["mask"] = np.expand_dims(mask, 0)
+        sample["trimap"] = np.expand_dims(trimap, 0)
+
+        return sample
