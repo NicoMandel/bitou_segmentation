2ccbb599f4345f3f64f83cc76c1809068ff577ba
Nico Mandel <mandel.nicolas@gmail.com>
2022-12-06 12:20:03 +0100

    adapted syncing script

--- scripts/rsync_dataset_to_cluster.sh
+++ scripts/rsync_dataset_to_cluster.sh
@@ -4,39 +4,46 @@
 # USAGE FUNCTION
 print_usage() {
     printf "\nThis bash script uses rsync to copy a dataset from your machine to
-    QUTs HPC cluster. Requires the names of the subfolder for images and masks.
-    The data will land in /work/quteagles/data_bitou. Please ensure you have access
-    to the /quteagles workspace by requesting it from the HPC.
+    QUTs HPC cluster. Requires the names of the subfolder for images and masks,
+    as well as the dataset name
+    The data will land in /work/quteagles/data_bitou/<name>, with subfolders <images> and <masks>
+    Please ensure you have access to the /quteagles workspace by requesting it from the HPC team.
+    To overwrite a folder, 
+    
 
 Example Usage:
     ./rsync_dataset_to_cluster.sh  -d <dataset> -u <username> -n <dataset_name>
 
 Input Arguments
-    -u <username>       your QUT username for HPC access. 
-    -d <dataset>        the dataset, in the ground-classification dataset format, to be copied across.
-    -n <dataset_name>   the unique name given to this dataset.
-                            
+    -u <username>       your QUT username for HPC access
+    -m <masks>          the masks folder
+    -i <input>          the input folder of raw images
+    -n <name>           the dataset name - which will be the name of the subfolder
+    -N                  flag for new dataset creation
 "
 }
 
 
 # PARSE ARGUMENTS
-dataset_path=''
 username=''
+image_path=''
+mask_path=''
 dataset_name=''
 new_dataset=false
-while getopts 'd:u:n:' flag; do
+while getopts 'u:m:i:n:N' flag; do
     case "${flag}" in
-        d) dataset_path="${OPTARG}" ;;
         u) username="${OPTARG}" ;;
+        i) image_path="${OPTARG}" ;;
+        m) mask_path="${OPTARG}" ;;
         n) dataset_name="${OPTARG}" ;;
+        N) new_dataset=true ;;
         *) print_usage
             exit -1 ;;
     esac
 done
 
-if [ -z "${dataset_path}" ]; then
-    echo "The script requires an input dataset."
+if [ -z "${image_path}" ]; then
+    echo "The script requires an input image folder."
     print_usage
     exit -1
 fi
@@ -47,36 +54,66 @@ if [ -z "${username}" ]; then
     exit -1
 fi
 
+if [ -z "${mask_path}" ]; then
+    echo "The script requires an input mask folder."
+    print_usage
+    exit -1
+fi
+
 if [ -z "${dataset_name}" ]; then
-    echo "The script requires the name of the dataset."
+    echo "The script requires a dataset name."
     print_usage
     exit -1
 fi
 
 # Setup Computer and directory
 cluster_pc="${username}@lyra.qut.edu.au"
-cluster_dir="/work/quteagles/data_bitou"
+cluster_dir="/work/quteagles/data_bitou/${dataset_name}"
+cluster_mask_dir="/work/quteagles/data_bitou/${dataset_name}/masks"
+cluster_img_dir="/work/quteagles/data_bitou/${dataset_name}/images"
+cluster_images=($(ssh ${cluster_pc} 'ls '${cluster_dir}'/images/*'))
+cluster_masks=($(ssh ${cluster_pc} 'ls '${cluster_dir}'/masks/*'))
+
+check_images() {
+    local local_images=("${image_path}/*")
+    local local_masks=("${mask_path}"/*)
+
+    local local_img="$(basename -- ${local_images[0]})"
+    local cluster_img="$(basename -- ${cluster_images[0]})"
+    local local_mask="$(basename -- ${local_masks[0]})"
+
+    # check if local image name and mask image name are the same
+    if [ ${local_img} != ${local_mask} ] ; then
+        echo "Images and Masks do not have the same names."
+        echo "Image 1 name: ${local_img}\tMask 1 name: ${cluster_img}"
+        exit -1
+    else
+        echo "Images and Masks have same name. Continuing"
+    fi
+    
+    # check if local images and cluster images are the same
+    if [ ${local_img} != ${cluster_img} ] ; then
+        echo "Local dataset ${image_path} and cluster dataset ${cluster_dir} are not the same."
+        echo "Local Image 1 name: ${local_img}\tCluster Image 1 name: ${cluster_img}"
+        exit -1
+    else
+        echo "Local Image Directory and Cluster Image Directory have same first file. Continuing."
+    fi
+}
 
 # See if directory exists on cluster machine - if not can do straight copy
 if ssh ${cluster_pc} '[ -d ${cluster_dir}]'; then
-    # get images on cluster and local machines, use these to check if correct dataset name was entered
-    local_images=("${dataset_path}/images/images"/*)
-    cluster_images=($(ssh ${cluster_pc} 'ls '${cluster_dir}'/images/images/*'))
-
-    local_img="$(basename -- ${local_images[0]})"
-    cluster_img="$(basename -- ${cluster_images[0]})"
-    if [ ${local_img} != ${cluster_img} ] ; then
-        echo "The local dataset ${dataset_path} and the cluster dataset ${cluster_dir} are not the same. Make sure you are specifying the correct local and cluster dataset variables."
-        echo "First image on local machine ${local_img}"
-        echo "First image on cluster machine ${cluster_img}"
-        exit -1
-    fi
+    check_images
 elif [ "${new_dataset}" = false ]; then # dataset doesn't exist, only run rsync command if -N flag is set
     echo "The ${dataset_name} does not exist on the cluster. If attempting to copy a new dataset make sure the -N flag is set."
     exit -1
 fi
 
 # Create rsync command
-command="rsync -azP --exclude pointclouds --exclude data_files --exclude .rosdata --exclude .label_ground ${dataset_path}/ ${cluster_pc}:${cluster_dir}"
-echo "Running Command: ${command}"
-eval ${command}
+command_imgs="rsync -azP ${image_path}/ ${cluster_pc}:${cluster_img_dir}"
+echo "Syncing Images with Command: ${command_imgs}"
+eval ${command_imgs}
+
+command_masks="rsync -azP ${mask_path}/ ${cluster_pc}:${cluster_mask_dir}"
+echo "Syncing Images with Command: ${command_masks}"
+eval ${command_masks}
