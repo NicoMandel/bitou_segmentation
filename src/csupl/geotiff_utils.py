b781195b74cc2053725cf352dc6c2c69eaaa3876
Nico Mandel <mandel.nicolas@gmail.com>
2023-02-06 17:19:09 +0100

    added geotiff utils for filename parsing

--- src/csupl/geotiff_utils.py
+++ src/csupl/geotiff_utils.py
@@ -1,7 +1,12 @@
 import numpy as np
+import os.path
+from pathlib import Path
 import rasterio
 from rasterio.plot import reshape_as_image, reshape_as_raster
 
+ALLOWED_EXTENSIONS = set(['tif', 'tiff', 'geotif', 'geotiff'])
+PROCESSED_PREFIX = "DNN"
+JOIN_CHAR = "_"
 
 def is_not_empty(window : np.ndarray, nodata_val : list = [255, 255, 255]) -> bool:
     img = reshape_as_image(window)
@@ -14,3 +19,12 @@ def convert_idx(w_width, w_height, cols, i):
     start_y = w_height * (i % cols)
     return start_x, start_y
 
+def get_tiff_files(path : str) -> list:
+    """
+        Function to get all .tiff file extensions in a folder
+        and exclude the ones with the DNN prefix
+    """
+    plPath = Path(path)
+    img_list = ([[x for x in plPath.glob(".".join(["*",fext]))] for fext in ALLOWED_EXTENSIONS])
+    img_l = [val for sublist in img_list for val in sublist if val.parts[-1].split(JOIN_CHAR)[0] != PROCESSED_PREFIX]
+    return img_l
