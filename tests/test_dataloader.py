b5d23e89fa01868f07cae6425b499985fad3bb75
Nico Mandel <mandel.nicolas@gmail.com>
2023-02-17 16:32:18 +0100

    added tests for reading images and geotiffs - channel order

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -6,13 +6,28 @@ import pytest
 import os.path
 import numpy as np
 from tqdm import tqdm
+import rasterio
+from rasterio.plot import reshape_as_image
 
 from csupl import dataloader
 from csupl.utils import load_image
 
+
+
 fdir = os.path.abspath(os.path.dirname(__file__))
 root = os.path.join(fdir, "..", "data")
 num_classes = 2
+
+def check_channel(img : np.ndarray, channel_id : int, value : int = 255):
+    for i in range(img.shape[2]):
+        if i == channel_id:
+            assert np.all(img[..., i] == value)
+        else:
+            assert np.all(img[..., i] == 0)
+    
+def check_channels(img: np.ndarray, channel_1 : int, channel_2 : int, value_1 : int = 128, value_2 : int = 128 /2):
+    assert np.all(img[..., channel_1] == value_1)
+    assert np.all(img[..., channel_2] == value_2)
 
 def test_dataset_length():
     ds = dataloader.BitouDataset(root = root, img_folder = "bitou_test", mask_folder = "bitou_test_masks", f_ext = ".JPG")
@@ -56,33 +71,60 @@ def test_mask_validity():
     assert cl_ct == 0
 
 def test_image_loading():
-    imgdir = os.path.join(fdir, "..", "tmp")
+    imgdir = os.path.join(fdir, "test_images")
     
-    def test_channel(img : np.ndarray, channel_id : int):
-        assert np.all(img[..., channel_id]) >= 250 
-    
-    def test_channels(img: np.ndarray, channel_1 : int, channel_2 : int):
-        assert img[..., channel_1].all() == 128
-        assert img[..., channel_2].all() == 128 / 2
-
-    r_jpg = load_image(os.path.join(imgdir, "r.jpg"))
+    # r_jpg = load_image(os.path.join(imgdir, "r.jpg"))
     r_png = load_image(os.path.join(imgdir, "r.png"))
-    test_channel(r_jpg, 0)
-    test_channel(r_png, 0)
+    # check_channel(r_jpg, 0)
+    check_channel(r_png, 0)
 
-    g_jpg = load_image(os.path.join(imgdir, "g.jpg"))
+    # g_jpg = load_image(os.path.join(imgdir, "g.jpg"))
     g_png = load_image(os.path.join(imgdir, "g.png"))
-    test_channel(g_jpg, 1)
-    test_channel(g_png, 1)
+    # check_channel(g_jpg, 1)
+    check_channel(g_png, 1)
 
-    b_jpg = load_image(os.path.join(imgdir, "b.jpg"))
+    # b_jpg = load_image(os.path.join(imgdir, "b.jpg"))
     b_png = load_image(os.path.join(imgdir, "b.png"))
-    test_channel(b_jpg, 2)
-    test_channel(b_png, 2)
+    # check_channel(b_jpg, 2)
+    check_channel(b_png, 2)
 
     mix1 = load_image(os.path.join(imgdir, "mix1.png"))
     mix2 = load_image(os.path.join(imgdir, "mix2.png"))
     mix3 = load_image(os.path.join(imgdir, "mix3.png"))
-    test_channels(mix1, 0, 1)
-    test_channels(mix2, 1, 2)
-    test_channels(mix3, 2, 0)
+    check_channels(mix1, 0, 1)
+    check_channels(mix2, 1, 2)
+    check_channels(mix3, 2, 0)
+
+def test_tif_loading():
+    imgdir = os.path.join(fdir, "test_images")
+
+    rfile = os.path.join(imgdir, "r.tif")
+    with rasterio.open(rfile, 'r') as src:
+        img = src.read()
+    check_channel(reshape_as_image(img), 0)
+
+    gfile = os.path.join(imgdir, "g.tif")
+    with rasterio.open(gfile, 'r') as src:
+        img = src.read()
+    check_channel(reshape_as_image(img), 1)
+    
+    bfile = os.path.join(imgdir, "b.tif")
+    with rasterio.open(bfile, 'r') as src:
+        img = src.read()
+    check_channel(reshape_as_image(img), 2)
+
+    # Mixes
+    mix1file = os.path.join(imgdir, "mix1.tif")
+    with rasterio.open(mix1file, 'r') as src:
+        mix1 = src.read()
+    check_channels(reshape_as_image(mix1), 0, 1)
+
+    mix2file = os.path.join(imgdir, "mix2.tif")
+    with rasterio.open(mix2file, 'r') as src:
+        mix2 = src.read()
+    check_channels(reshape_as_image(mix2), 1, 2)
+
+    mix3file = os.path.join(imgdir, "mix3.tif")
+    with rasterio.open(mix3file, 'r') as src:
+        mix3 = src.read()
+    check_channels(reshape_as_image(mix3), 2, 0)
